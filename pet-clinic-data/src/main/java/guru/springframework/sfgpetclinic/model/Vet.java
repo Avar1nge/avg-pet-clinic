f9325109ce6ebf012b715b2d64f05b40cb08e285
aleksandrparhomchuk <aleksandrparhomchuk@gmail.com>
2021-08-25 00:54:36 +0300

    Erhance Owners with contact info and Pets on startup closes #18

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -7,5 +8,13 @@ import java.util.Set;
  */
 public class Vet extends Person {
 
-  private Set<Speciality> specialities;
+  private Set<Speciality> specialities = new HashSet<>();
+
+  public Set<Speciality> getSpecialities() {
+    return specialities;
+  }
+
+  public void setSpecialities(Set<Speciality> specialities) {
+    this.specialities = specialities;
+  }
 }
