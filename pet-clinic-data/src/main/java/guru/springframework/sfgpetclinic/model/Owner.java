162fb35151b10ec42a7f5841a3b06e1d52ef89c4
aleksandrparhomchuk <aleksandrparhomchuk@gmail.com>
2021-08-26 01:00:36 +0300

    Convert Owners to JPA Entities. Closes #21

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,17 +1,27 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * Created by jt on 7/13/18.
- */
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+  @Column(name = "city")
   private String city;
+
+  @Column(name = "address")
   private String address;
+
+  @Column(name = "telephone")
   private String telephone;
 
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
   private Set<Pet> pets = new HashSet<>();
 
   public String getCity() {
